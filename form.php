8e0b95f00a8fc5f2ba02ab2ca6ef2476cec4f787
Maciej Dubiel <73096946+pozdromaciek@users.noreply.github.com>
2025-03-10 11:42:50 +0100

    added php, changed images

---
+++ form.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $phone = htmlspecialchars($_POST["name"]);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $message = htmlspecialchars($_POST["message"]);
+    $form_subject = htmlspecialchars($_POST["subject"]);
+
+    $to = "twoj@email.com";  // Podmień na swój adres e-mail
+    $subject = "Formularz kontaktowy: $form_subject";
+    $body = "Done kontaktowe:\n
+    Imię i nazwisko: $name\n
+    Email: $email\n
+    Numer kontaktowy: $phone\n
+    \nTreść wiadomości:\n$message";
+
+    $headers = "From: $email\r\nReply-To: $email\r\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Wiadomość została wysłana!";
+    } else {
+        echo "Błąd podczas wysyłania wiadomości.";
+    }
+} else {
+    echo "Nieprawidłowa metoda żądania.";
+}
+?>
